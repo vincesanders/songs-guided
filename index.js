234c0192b0608725a9bd7c01342502645f6ceeb4
Vincent Sanders <vincesanders@hotmail.com>
2020-02-13 12:30:01 -0600

    Made port dynamic.

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ server.get('/', (req, res) => {
     res.status(200).json(songs);
 })
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Server is listening on port ${port}`))
